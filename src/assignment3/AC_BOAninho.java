d6d1535cc0d5c36dde8bd42fd2505bf764ca16e9
StefanvdSman <sj.vandersman@gmail.com>
2021-03-24 16:59:12 +0100

    add getName method to acceptance strategy

--- src/assignment3/AC_BOAninho.java
+++ src/assignment3/AC_BOAninho.java
@@ -1,5 +1,24 @@
 package assignment3;
 
-public class AC_BOAninho {
+import java.util.Map;
+import genius.core.bidding.BidDetails;
+import genius.core.boaframework.AcceptanceStrategy;
+import genius.core.boaframework.Actions;
+import genius.core.boaframework.NegotiationSession;
+import genius.core.boaframework.OfferingStrategy;
+import genius.core.boaframework.OpponentModel;
+import negotiator.boaframework.sharedagentstate.anac2011.GahboninhoSAS;
 
+public class AC_BOAninho extends AcceptanceStrategy {
+
+	@Override
+	public Actions determineAcceptability() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getName() {
+		return "2021 - Boaninho";
+	}
 }
